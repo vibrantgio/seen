ddd871c0e3791458bffff6552a64afd10c69e675
René Post <git@xpt.nl>
2021-04-08 21:35:29 +0200

    Simplify projection code

--- projection.go
+++ projection.go
@@ -2,12 +2,10 @@ package seen
 
 import "github.com/reactivego/seen/transform"
 
-var defaultProjection = transform.Projection{R: 1, T: 1, N: 1, F: 100}
-
 // DefaultPerspectiveProjection creates a perspective projection matrix from
 // the default frustrum.
-var DefaultPerspectiveProjection = Matrix{defaultProjection.PerspectiveMat4x4()}
+var DefaultPerspectiveProjection = Matrix{transform.Frustum(1, 1, 1, 100)}
 
 // DefaultOrthographicProjection creates an orthographic projection matrix from
 // the default frustrum.
-var DefaultOrthographicProjection = Matrix{defaultProjection.OrthographicMat4x4()}
+var DefaultOrthographicProjection = Matrix{transform.Ortho(1, 1, 1, 100)}
--- transform/projection.go
+++ transform/projection.go
@@ -8,28 +8,27 @@ type Projection struct {
 	R, T, N, F float64
 }
 
-func Frustrum(fovy, aspect, near, far float64) Projection {
-	t := math.Tan(0.5*fovy) * near
-	r := t * aspect
-	return Projection{r, t, near, far}
+func Frustum(r, t, n, f float64) Mat4x4 {
+	return Projection{r, t, n, f}.PerspectiveMat4x4()
 }
 
-// Perspective will project points inside the cube specified by l,r,b,t,n,f = -r,r,-t,t,-n,-f
-// to clip coordinates. For all valid (non clipped) coordinates the following condition
-//  holds: -wc < xc,yc,zc < wc
+func Ortho(r, t, n, f float64) Mat4x4 {
+	return Projection{r, t, n, f}.OrthographicMat4x4()
+}
+
+func Perspective(fovy, aspect, near, far float64) Mat4x4 {
+	t := math.Tan(0.5*fovy) * near
+	r := t * aspect
+	return Projection{r, t, near, far}.PerspectiveMat4x4()
+}
+
+// PerspectiveMat4x4 will return a matrix capabable of projecting points inside the cube
+// specified by l,r,b,t,n,f = -r,r,-t,t,-n,-f to clip coordinates. For all valid (non clipped)
+// coordinates the following condition holds: -wc < xc,yc,zc < wc
 // All coordinates for which this condition doesn't hold need to be clipped.
 // To get from clip space coordinates to native device coordinates divide the xc,yc,zc by wc.
 // So xn,yn,zn = xc/wc,yc/wc,zc/wc will give coordinates in de range [-1,1]. These need to be
 // mapped via a viewport to screen coordinates.
-func (p Projection) Perspective(xe, ye, ze float64) (xc, yc, zc, wc float64) {
-	r, t, n, f := p.R, p.T, p.N, p.F
-	xc = xe * n / r
-	yc = ye * n / t
-	zc = (ze*(f+n) + 2*f*n) / (n - f)
-	wc = -ze
-	return
-}
-
 func (p Projection) PerspectiveMat4x4() Mat4x4 {
 	r, t, n, f := p.R, p.T, p.N, p.F
 	return Mat4x4{
@@ -40,14 +39,6 @@ func (p Projection) PerspectiveMat4x4() Mat4x4 {
 	}
 }
 
-func (p Projection) Orthographic(xe, ye, ze float64) (xc, yc, zc float64) {
-	r, t, n, f := p.R, p.T, p.N, p.F
-	xc = xe / r
-	yc = ye / t
-	zc = (ze*-2 - (f + n)) / (f - n)
-	return
-}
-
 func (p Projection) OrthographicMat4x4() Mat4x4 {
 	r, t, n, f := p.R, p.T, p.N, p.F
 	return Mat4x4{
@@ -56,25 +47,4 @@ func (p Projection) OrthographicMat4x4() Mat4x4 {
 		0, 0, 2 / (n - f), (f + n) / (n - f),
 		0, 0, 0, 1,
 	}
-}
-
-type AsymmetricProjection struct {
-	L, R, B, T, N, F float64
-}
-
-func (p AsymmetricProjection) Perspective(xe, ye, ze float64) (xc, yc, zc, wc float64) {
-	l, r, b, t, n, f := p.L, p.R, p.B, p.T, p.N, p.F
-	xc = (xe*2*n + ze*(r+l)) / (r - l)
-	yc = (ye*2*n + ze*(t+b)) / (t - b)
-	zc = (ze*(f+n) + 2*f*n) / (n - f)
-	wc = -ze
-	return
-}
-
-func (p AsymmetricProjection) Orthographic(xe, ye, ze float64) (xc, yc, zc float64) {
-	l, r, b, t, n, f := p.L, p.R, p.B, p.T, p.N, p.F
-	xc = (xe*2 - (r + l)) / (r - l)
-	yc = (ye*2 - (t + b)) / (t - b)
-	zc = (ze*-2 - (f + n)) / (f - n)
-	return
 }
