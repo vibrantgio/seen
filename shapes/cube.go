b6b09aea62d1fd2fdfbf6e4e123a05eccff381d3
René Post <git@xpt.nl>
2021-04-11 11:17:50 +0200

    Refactor coordinate maps

---
+++ shapes/cube.go
@@ -0,0 +1,37 @@
+package shapes
+
+import "github.com/reactivego/seen"
+
+// CubePoints form a 2x2x2 cube, centered on the origin.
+var CubePoints = [...]seen.Point{
+	{X: -1, Y: -1, Z: -1},
+	{X: -1, Y: -1, Z: 1},
+	{X: -1, Y: 1, Z: -1},
+	{X: -1, Y: 1, Z: 1},
+	{X: 1, Y: -1, Z: -1},
+	{X: 1, Y: -1, Z: 1},
+	{X: 1, Y: 1, Z: -1},
+	{X: 1, Y: 1, Z: 1},
+}
+
+// UnitCubePoints form a 1x1x1 cube from the origin [0,0,0] to [1, 1, 1].
+var UnitCubePoints = [...]seen.Point{
+	{X: 0, Y: 0, Z: 0},
+	{X: 0, Y: 0, Z: 1},
+	{X: 0, Y: 1, Z: 0},
+	{X: 0, Y: 1, Z: 1},
+	{X: 1, Y: 0, Z: 0},
+	{X: 1, Y: 0, Z: 1},
+	{X: 1, Y: 1, Z: 0},
+	{X: 1, Y: 1, Z: 1},
+}
+
+// Map to points in the surfaces of a cube
+var CubeCoordinateMap = [...][]int{
+	{0, 1, 3, 2}, // left
+	{5, 4, 6, 7}, // right
+	{1, 0, 4, 5}, // bottom
+	{2, 3, 7, 6}, // top
+	{3, 1, 5, 7}, // front
+	{0, 2, 6, 4}, // back
+}
---
+++ shapes/icosahedron.go
@@ -0,0 +1,44 @@
+package shapes
+
+import "github.com/reactivego/seen"
+
+const IcoXX = 0.525731112119133606
+const IcoXZ = 0.850650808352039932
+
+var IcosahedronPoints = [...]seen.Point{
+	{X: -IcoXX, Y: 0.0, Z: -IcoXZ},
+	{X: IcoXX, Y: 0.0, Z: -IcoXZ},
+	{X: -IcoXX, Y: 0.0, Z: IcoXZ},
+	{X: IcoXX, Y: 0.0, Z: IcoXZ},
+	{X: 0.0, Y: IcoXZ, Z: -IcoXX},
+	{X: 0.0, Y: IcoXZ, Z: IcoXX},
+	{X: 0.0, Y: -IcoXZ, Z: -IcoXX},
+	{X: 0.0, Y: -IcoXZ, Z: IcoXX},
+	{X: IcoXZ, Y: IcoXX, Z: 0.0},
+	{X: -IcoXZ, Y: IcoXX, Z: 0.0},
+	{X: IcoXZ, Y: -IcoXX, Z: 0.0},
+	{X: -IcoXZ, Y: -IcoXX, Z: 0.0},
+}
+
+var IcosahedronCoordinateMap = [...][]int{
+	{0, 4, 1},
+	{0, 9, 4},
+	{9, 5, 4},
+	{4, 5, 8},
+	{4, 8, 1},
+	{8, 10, 1},
+	{8, 3, 10},
+	{5, 3, 8},
+	{5, 2, 3},
+	{2, 7, 3},
+	{7, 10, 3},
+	{7, 6, 10},
+	{7, 11, 6},
+	{11, 0, 6},
+	{0, 1, 6},
+	{6, 1, 10},
+	{9, 0, 11},
+	{9, 11, 2},
+	{9, 2, 5},
+	{7, 2, 11},
+}
